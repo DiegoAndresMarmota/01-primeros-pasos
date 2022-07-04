d6d0f1616c054e3d0cf601abbbe012197f2b96e2
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-07-04 15:11:19 -0400

    Evento doble click

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -6,12 +6,24 @@ export const EventosComponente = () => {
         alert("Has dado click al boton!" + nombre);
     }
 
+    function hasDadoDobleClick() {
+        alert("Has dado doble click!");
+    }
+
   return (
     <div>
         <h1>Eventos en React</h1>
 
-        {/*Evento Click*/}
-        <button onClick={ e => hasDadoClick(e, "Diego") }>Dame un click!</button>
+          <p>
+            {/*Evento Click*/}
+            <button onClick={e => hasDadoClick(e, "Diego")}>Dame un click!</button>
+          </p>
+            
+          <p>
+            {/*Evento Doble click*/}
+            <button onDoubleClick={ hasDadoDobleClick }>Dame doble click!</button>
+          </p>
+        
     </div>
   )
 }
